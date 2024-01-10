06b8dd30627aa5e0774dabf9c8c0bf03cf2599ec
Hajin74 <yoonmij9974@naver.com>
2024-01-11 01:54:51 +0900

    chore: BoardController

--- src/main/java/project/first/board/BoardController.java
+++ src/main/java/project/first/board/BoardController.java
@@ -1,10 +1,7 @@
 package project.first.board;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
