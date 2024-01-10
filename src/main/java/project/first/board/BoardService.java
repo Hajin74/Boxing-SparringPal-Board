30346f2881a4f0782637a6cf1614169e71d9858f
Hajin74 <yoonmij9974@naver.com>
2024-01-10 18:20:08 +0900

    feat: BoardService 개발

--- src/main/java/project/first/board/BoardService.java
+++ src/main/java/project/first/board/BoardService.java
@@ -1,9 +1,33 @@
 package project.first.board;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class BoardService {
 
+    private final BoardRepository boardRepository;
+
+    // 게시판 생성
+    @Transactional
+    public Long create(Board board) {
+        boardRepository.save(board);
+        return board.getId();
+    }
+
+    // 게시판 전체 조회
+    public List<Board> findAll() {
+        return boardRepository.findAll();
+    }
+
+    // 게시판 개별 조회
+    public Board findOne(Long boardId) {
+        return boardRepository.findById(boardId);
+    }
 
 }
