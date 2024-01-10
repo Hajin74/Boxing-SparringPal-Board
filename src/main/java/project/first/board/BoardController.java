d32190401c832819deff814b2cf4952af5664b38
Hajin74 <yoonmij9974@naver.com>
2024-01-11 01:05:06 +0900

    feat: BoardController 개발 (전체조회, 개별조회)

--- src/main/java/project/first/board/BoardController.java
+++ src/main/java/project/first/board/BoardController.java
@@ -1,8 +1,12 @@
 package project.first.board;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -11,5 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping
+    public List<Board> getAllBoard() {
+        return boardService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Board getBoardById(@PathVariable("id") Long id) {
+        return boardService.findOne(id);
+    }
 
 }
