1e0142e17df9bdcd0629d2d2d3fa4437e026e5ab
Hajin74 <yoonmij9974@naver.com>
2024-01-12 01:03:13 +0900

    feat: board 테스트코드 (createBoardTest)

--- src/test/java/project/first/board/BoardServiceTest.java
+++ src/test/java/project/first/board/BoardServiceTest.java
@@ -3,6 +3,7 @@ package project.first.board;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,16 +19,22 @@ public class BoardServiceTest {
     BoardRepository boardRepository;
 
     @Test
-    public void createTest() throws Exception {
+    @Rollback(false)
+    public void createBoardTest() throws Exception {
         // given
         Board board = new Board();
         board.setTitle("게시판 테스트");
 
         // when
         Long boardId = boardService.create(board);
+        board.setId(boardId);
+
+        // 엔티티끼리는 각자 생성해서 주기 때문에 다른것을 가리킴 -> 음.. 
+        System.out.println(board);
+        System.out.println(boardRepository.findById(boardId));
 
         // then
-        assertEquals(board, boardRepository.findById(boardId));
+        assertEquals(boardId, boardRepository.findById(boardId).getId());
     }
 
 }
