a74fd6e806aa1acdf0a662ba608e04418d995262
Hajin74 <yoonmij9974@naver.com>
2024-01-13 02:12:39 +0900

    feat: post controller 조회 추가

--- src/main/java/project/first/post/PostController.java
+++ src/main/java/project/first/post/PostController.java
@@ -1,4 +1,30 @@
 package project.first.post;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public List<Post> getAllPost() {
+        return postService.findAll();
+    }
+
+    @GetMapping("/{user}")
+    public List<Post> getPostByUser(@PathVariable("user") String user) {
+        return postService.findByUser(user);
+    }
+
+    @GetMapping("/{title}")
+    public List<Post> getPostByTitle(@PathVariable("title") String title) {
+        return postService.findByTitle(title);
+    }
+
 }
