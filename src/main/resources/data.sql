c00472311c21cb4ca42a0bcbe16a2c74ade31ccf
Hajin74 <yoonmij9974@naver.com>
2024-01-18 01:10:22 +0900

    feat: 더미데이터 넣기

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,7 @@ insert into board (title) values ("주말 스파링 게시판");
 insert into board (title) values ("평일 스파링 게시판");
 insert into board (title) values ("직장인 스파링 게시판");
 insert into board (title) values ("여자 스파링 게시판");
+insert into board (title) values ("남자 스파링 게시판");
 
 insert into post (board_id, created_at, content, title, status)
 values (1, "2024-01-14 21:27:36", "스파링하고 같이 점심먹어요!", "강남권 사시는 분~", "RECRUITING");
